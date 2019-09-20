6179bb89b3cc6036e65c766966bfad72fab90f92
syed4.hussain <syed4.hussain@paytm.com>
2019-09-20 18:53:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var request = require("request")
+
+var app = express();
+
+
+app.set("view engine", 'ejs');
+
+app.get("/", function(req, res){
+    res.render("welcome");
+});
+
+app.get("/results", function(req, res){
+    var q = req.query.search;
+    var url = "http://omdbapi.com/?s=" + q + "&apikey=thewdb";
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("result", {data:data});
+
+        }
+    })
+});
+
+app.get("/rating", function(req, res){
+    var q = req.query.id;
+    var url = "http://www.omdbapi.com/?i=" + q +"&apikey=thewdb";
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("rating", {data:data});
+        }
+    })
+});
+
+app.listen(3001, function(){
+    console.log("Server is started");
+});
